4e76eac231e35c2c82d0aa9f1db7af8cea6fc024
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2023-03-24 23:24:29 +0200

    upr3: add student info

---
+++ StudentInfoSystem/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfoSystem
+{
+    internal class Student
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+
+        public string Faculty { get; set; }
+        public string Speciality { get; set; }
+        public string Degree { get; set; }
+        public string Status { get; set; }
+
+        public string FacultyNumber { get; set; }
+        public int Course { get; set;}
+        public int Stream { get; set; }
+        public int GroupNumber { get; set; }
+
+    }
+}
